6117090ca731c16ef7a16fafdda249cc2133dd87
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-17 13:01:33 -0300

    Configure HATEOAS

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into usuarios (email, enabled, first_name, last_name, password, user_name) values ('emitchley0@delicious.com', true, 'Emmey', 'Mitchley', '84TYWDUW632', 'emitchley0');
+insert into usuarios (email, enabled, first_name, last_name, password, user_name) values ('aleteurtre1@1688.com', true, 'Archie', 'Leteurtre', 'HoV5kSM3Xy', 'aleteurtre1');
+
