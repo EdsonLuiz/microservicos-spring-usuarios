43e0bf48d222862ee819982d5034f1d1b71b50ee
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-17 09:29:30 -0300

    Start project

---
+++ src/main/java/com/edson/app/usuarios/ServicoUsuariosApplication.java
@@ -0,0 +1,13 @@
+package com.edson.app.usuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoUsuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoUsuariosApplication.class, args);
+	}
+
+}
