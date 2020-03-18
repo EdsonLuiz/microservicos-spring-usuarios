62bd2828395580dd9cb39c82d9966b4a0bbce2f3
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2020-03-18 11:09:19 -0300

    Define path for rest resource of UsuarioRepository

--- src/main/java/com/edson/app/usuarios/models/repositories/UsuarioRepository.java
+++ src/main/java/com/edson/app/usuarios/models/repositories/UsuarioRepository.java
@@ -2,13 +2,16 @@ package com.edson.app.usuarios.models.repositories;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.edson.app.usuarios.models.entities.Usuario;
 
 @RepositoryRestResource(path = "usuarios")
 public interface UsuarioRepository extends PagingAndSortingRepository<Usuario, Long> {
-	public Usuario findByUserName(String userName);
+	@RestResource(path = "findusername")
+	public Usuario findByUserName(@Param("name") String userName);
 	
 	@Query("select u from Usuario u where u.userName=?1")
 	public Usuario getByUsername(String username);
